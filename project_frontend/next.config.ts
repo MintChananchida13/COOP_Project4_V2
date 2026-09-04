7d0bee272cb815d0e0c18d474d5ecf94b476fb09
MintChananchida13 <chananchida.somnuan13@gmail.com>
2026-09-04 20:47:23 +0700

    fix: proxy backend through vercel

--- project_frontend/next.config.ts
+++ project_frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://107.129.186.30:62100/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
